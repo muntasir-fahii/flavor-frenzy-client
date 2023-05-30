419d41db81f6626ee652cedae07afa0ac8a6c54e
muntasir-fahim <fa3877957@gmail.com>
2023-05-31 01:59:57 +0600

    first commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+const HoePage = () => {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+};
+
+export default HoePage;
---
+++ data/sliderImages.ts
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Plate Of Fries and Burger",
+    headline: "Taste the Juicy Goodness",
+    paragraph:
+      "Indulge in our mouthwatering burger made with premium ingredients.",
+    cta: "Order Now",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/3053082/pexels-photo-3053082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Gourmet Pizza",
+    headline: "Experience Pizza Perfection",
+    paragraph:
+      "Savor the flavors of Italy with our authentic, handcrafted pizza.",
+    cta: "Try now",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/7741852/pexels-photo-7741852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Grilled Meat on Brown Wooden Chopping  Steak",
+    headline: "Satisfy Your Cravings",
+    paragraph:
+      "Delight in the tenderness and flavor of our perfectly grilled steak.",
+    cta: "Explore Menu",
+  },
+];
